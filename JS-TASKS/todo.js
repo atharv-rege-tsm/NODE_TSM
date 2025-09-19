7a3ba9fb5255c245e0957601016760e8b894129f
AtharvRaoRege <atharvrege722@gmail.com>
2025-09-19 19:47:06 +0530

    Remove folders from the remote - unwanted

--- JS-TASKS/todo.js
+++
@@ -1,9 +0,0 @@
-let html = '';
-let title = document.querySelector("#task-input");
-document.querySelector("#submit").addEventListener("click", () => {
-    if (title.value != "") {
-        html += `<div id="todo-item">${title.value}</div>`
-        document.querySelector('#todo-container').innerHTML = html;
-        title.value = "";
-    }
-})
